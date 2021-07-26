25906fb104645a9ac70ef3a84bb075ed3237d6bf
Marcos Allan <marcos.allan@casaamerica.com.br>
2021-07-26 14:38:35 -0300

    adicionando arquivos

---
+++ dist/database/seeds/20210725010517-cria-usuarios.js
@@ -0,0 +1,38 @@
+"use strict";const bcryptjs = require('bcryptjs');
+
+module.exports = {
+  up: async (queryInterface) => queryInterface.bulkInsert('users',
+    [
+      {
+        nome: 'Marquinhos',
+        email: 'mm@gmail.com',
+        password_hash: await bcryptjs.hash('123456', 8),
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        nome: 'Junior',
+        email: 'jj@gmail.com',
+        password_hash: await bcryptjs.hash('123456', 8),
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        nome: 'Alice',
+        email: 'aa@gmail.com',
+        password_hash: await bcryptjs.hash('123456', 8),
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        nome: 'Amanda',
+        email: 'am@gmail.com',
+        password_hash: await bcryptjs.hash('123456', 8),
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+    ],
+    {}),
+
+  down: () => {},
+};
--- server.js
+++
@@ -1,8 +0,0 @@
-import app from './app';
-
-const port = 3001;
-app.listen(port, () => {
-  console.log();
-  console.log(`Escutando na porta ${port}`);
-  console.log(`CRTL + Clique em http://localhost:${port}`);
-});
